5367d462e738aefb6e8db6db377a9689f531711d
Sophie Kim <270010565@bun2-206-17.local>
2019-05-13 11:17:32 +1200

    remove console.log

--- js/script.js
+++ js/script.js
@@ -16,7 +16,6 @@ function findMax(aNums) {
 //
 //Input: Get 3 grades for each student
 var iSize = parseInt(prompt('Class size?'));
-console.log(iSize);
 var aGradesPhysics = [];
 var aGradesChemistry = [];
 var aGradesBiology = [];
@@ -26,28 +25,27 @@ var aGradesBiology = [];
 for(var iCount=0; iCount<iSize; iCount++) {
     var iGradePhysics = parseInt(prompt('Student '+(iCount+1)+': Grade for Physics out of 100'));
     aGradesPhysics.push(iGradePhysics);
-    console.log(iGradePhysics);
+
 
     var iGradeChemistry = parseInt(prompt('Student '+(iCount+1)+': Grade for Chemistry out of 100'));
     aGradesChemistry.push(iGradeChemistry);
-      console.log(iGradeChemistry);
+
 
     var iGradeBiology = parseInt(prompt('Student '+(iCount+1)+': Grade for Biology out of 100'));
     aGradesBiology.push(iGradeBiology);
-      console.log(iGradeBiology);
+
 }
 
 // processing: calculate average grade for each student and find top grade in class
 var aAverageGrades = [];
-console.log(aAverageGrades);
 
 for(var iCount=0; iCount<iSize; iCount++) {
     var fAverage = (aGradesPhysics[iCount] + aGradesChemistry[iCount] + aGradesBiology[iCount]) / aGradesPhysics.length;
     aAverageGrades.push(fAverage);
-      console.log(fAverage);
+
 }
 var iTopGrade = findMax(aAverageGrades);
-  console.log(iTopGrade);
+
 
 //output: list result for each student and top grade
 
